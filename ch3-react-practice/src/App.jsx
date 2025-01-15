12d5b11fcc261cf23e64316cf35c5caa2d1bd50b
Yuu-Yur <gkcjdqls@gmail.com>
2025-01-15 14:30:49 +0900

    ch3 완료, ch4 준비

---
+++ ch3-react-practice/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Say from './component/Say'
+
+function App() {
+  return (
+    <>
+    <h1 className='react'> ch3 </h1>
+    <Say />
+    </>
+  )
+}
+
+export default App
---
+++ ch4-react-practice/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Say from './component/Say'
+import EventPractice from './component/EventPractice'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='react'>ch4 이벤트 핸들러</h1>
+      <Say />
+      <EventPractice />
+    </>
+  )
+}
+
+export default App
