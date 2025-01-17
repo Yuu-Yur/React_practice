89310d021fd7cf1c89548e8b185d3a3ea6424561
Yuu-Yur <gkcjdqls@gmail.com>
2025-01-17 11:08:35 +0900

    6장 복습,7장준비

--- ch6-react-practice/src/component/IterationPractice.jsx
+++ ch6-react-practice/src/component/IterationPractice.jsx
@@ -32,10 +32,11 @@ const IterationPractice = () => {
   // 응용문제 2: 항목 정렬 기능 추가
   // 응용문제 3: 삭제 취소 기능 추가 를 위한 메뉴 보이는 이벤트 핸들러, 메뉴 클릭 시 핸들러, 각각에 필요한 state
   const [showMenu, setShowMenu] = useState(false);
+  const [updateInput, setUpdateInput] = useState('');
   const [menuPosition, setMenuPosition] = useState({ top: 0, left: 0 });
   const [selected, setSelected] = useState({ id: 0, text: '' });
   const [sort, setSort] = useState('asc');
-  const [deleted, setDeleted] = useState({ id: 0, text: '' });
+  const [deleted, setDeleted] = useState([{ id: 0, text: '' }]);
   const handleContextMenu = (e, id, text) => {
     e.preventDefault();
     setMenuPosition({
@@ -63,28 +64,33 @@ const IterationPractice = () => {
     setShowMenu(false);
   };
 
-  //응용문제 1: 항목 수정 기능 추가
+  //응용문제 1: 항목 수정 기능 추가 나는 삭제 추가 했는데 이러면 id 가 바뀜, 이럴게 아니라 map 해서 삼항연산자로 찾아서 덮어씌워야함
   const onUpdate = () => {
     if (
-      names.some((name) => name.text === inputText) ||
-      inputText.trim() === ''
+      names.some((name) => name.text === updateInput) ||
+      updateInput.trim() === ''
     ) {
       return;
     }
-    onRemove();
-    onClick();
+    const updateNames = names.map((name) =>
+      name.id === selected.id ? { ...name, text: updateInput } : name,
+    );
+    setNames(updateNames);
+    setUpdateInput('');
   };
-  // 응용문제 2: 항목 정렬 기능 추가
+  // 응용문제 2: 항목 정렬 기능 추가 localeCompare 배움 a~b 면 오름차순, b~a 면 내림차순  - 를 쓸땐 b-a 가 오름차순인것의 반대
   const onSort = () => {
     let sortedNames;
     switch (sort) {
       case 'asc':
-        sortedNames = names.sort((a, b) => b.id - a.id);
+        // sortedNames = names.sort((a, b) => b.id - a.id);
+        sortedNames = names.sort((a, b) => a.text.localeCompare(b.text));
         setSort('desc');
         setNames(sortedNames);
         break;
       case 'desc':
-        sortedNames = names.sort((a, b) => a.id - b.id);
+        // sortedNames = names.sort((a, b) => a.id - b.id);
+        sortedNames = names.sort((a, b) => b.text.localeCompare(a.text));
         setSort('asc');
         setNames(sortedNames);
         break;
@@ -100,6 +106,7 @@ const IterationPractice = () => {
   };
 
   const onChange = (e) => setInputText(e.target.value);
+  const handleChangeUpdate = (e) => setUpdateInput(e.target.value);
 
   const onClick = () => {
     // 문제 1: 중복 항목 추가 방지 기능 구현 객체이므로 include 보다는 some() 사용
@@ -127,9 +134,12 @@ const IterationPractice = () => {
   };
 
   const onRemove = () => {
-    if (!confirm('삭제하시겠습니까?')) return;
+    if (!confirm(`${selected.text} 을/를 삭제하시겠습니까?`)) return;
     const nextNames = names.filter((name) => name.id !== selected.id);
-    setDeleted({ id: selected.id, text: selected.text });
+    // concat 은 더한걸 복사
+    setDeleted((prev) => prev.concat({ id: selected.id, text: selected.text }));
+    // {스프레드연산자객체, key:value} 는 덮어쓰기 , [스프레드연산자객체,{key:value,key:value}] 는 추가하기
+    setDeleted([...deleted, { id: selected.id, text: selected.text }]);
     setNames(nextNames);
   };
 
@@ -140,6 +150,9 @@ const IterationPractice = () => {
 
   const namesList = names.map((name) => (
     <li
+      style={{
+        marginBottom: 3,
+      }}
       key={name.id}
       //   onDoubleClick={() => onRemove(name.id)}
       // 문제 3: 항목의 ID 값을 클릭하여 확인하기 이벤트 리스너
@@ -150,15 +163,27 @@ const IterationPractice = () => {
     </li>
   ));
 
+  const deletedList = deleted.map((deleted) => (
+    <li key={deleted.id}>{deleted.text}</li>
+  ));
+
   return (
     <>
       <input
         value={inputText}
         onChange={onChange}
-        placeholder="수정하거나 추가할 항목을 입력하세요"
+        placeholder="추가할 항목을 입력하세요"
       />
       <button onClick={onClick}>추가</button>
-      <ul>{namesList}</ul>
+      <ul
+        style={{
+          listStyleType: 'none',
+          paddingBottom: 3,
+        }}
+      >
+        {namesList}
+      </ul>
+      <ul>{deletedList}</ul>
 
       {/*   응용문제 1: 항목 수정 기능 추가
             응용문제 2: 항목 정렬 기능 추가
@@ -179,6 +204,13 @@ const IterationPractice = () => {
               boxShadow: '0px 4px 8px rgba(0,0,0,0.2)',
             }}
           >
+            <li>
+              <input
+                type="text"
+                placeholder="수정할 항목을 입력하세요"
+                onChange={handleChangeUpdate}
+              />
+            </li>
             <li onClick={(e) => handleMenu(e.target.innerText)}>수정</li>
             <li onClick={(e) => handleMenu(e.target.innerText)}>삭제</li>
             <li onClick={(e) => handleMenu(e.target.innerText)}>정렬</li>
