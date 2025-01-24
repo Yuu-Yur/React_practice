004f76430c800335b0ea60b81e835a97ee315193
Yuu-Yur <gkcjdqls@gmail.com>
2025-01-24 17:43:36 +0900

    15장 복습필요

--- ch15-react-practice/src/App.jsx
+++ ch15-react-practice/src/App.jsx
@@ -1,10 +1,19 @@
 import {} from 'react';
 import './App.css';
+import ColorBox from './components/ColorBox';
+import SelectColors from './components/SelectColors';
+import ColorContext, { ColorProvider } from './contexts/color';
 
 function App() {
   return (
     <>
       <h1>CH15. context API</h1>
+      <ColorProvider>
+        <div>
+          <SelectColors />
+          <ColorBox />
+        </div>
+      </ColorProvider>
     </>
   );
 }
